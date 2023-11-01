5b84d347d66a3249d34b9e2c631f0b81f2c8050a
estherTran26 <mtuyenucalgary@gmail.com>
2023-11-01 16:31:35 -0600

    implement interface

--- src/com/thelocalmarketplace/software/SelfCheckoutStationLogic.java
+++ src/com/thelocalmarketplace/software/SelfCheckoutStationLogic.java
@@ -1,17 +1,24 @@
 package com.thelocalmarketplace.software;
 
+import com.jjjwelectronics.IDevice;
+import com.jjjwelectronics.IDeviceListener;
 import com.jjjwelectronics.scanner.Barcode;
+import com.jjjwelectronics.scanner.BarcodeScannerListener;
+import com.jjjwelectronics.scanner.BarcodedItem;
+import com.jjjwelectronics.scanner.IBarcodeScanner;
 import com.thelocalmarketplace.hardware.BarcodedProduct;
 import com.thelocalmarketplace.hardware.SelfCheckoutStation;
 import com.thelocalmarketplace.hardware.external.ProductDatabases;
 
-public class SelfCheckoutStationLogic {
+public class SelfCheckoutStationLogic implements BarcodeScannerListener{
 	private static boolean sessionStarts = false;
 	private static SelfCheckoutStation station;
 	private static double expectedWeight = 0;
+	private Barcode barcode;
 	
 	public SelfCheckoutStationLogic(SelfCheckoutStation st) {
 		station = st;
+		station.scanner.register(this);
 	}
 	
 	public boolean simulatedStartButtonEvent() {
@@ -23,7 +30,7 @@ public class SelfCheckoutStationLogic {
 		if(simulatedStartButtonEvent()) sessionStarts = true;
 	}
 	
-	public void addItemByBarcodeScan(Barcode barcode) throws Exception {
+	public void addItemByBarcodeScan() throws Exception {
 		if(!sessionStarts) throw new Exception("Cannot add item before starting a session!");
 		
 		BarcodedProduct addedProd = null;
@@ -33,7 +40,35 @@ public class SelfCheckoutStationLogic {
 		}
 		
 		if(addedProd == null) throw new Exception("Product not found!");
+	}
+
+	@Override
+	public void aDeviceHasBeenEnabled(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void aDeviceHasBeenDisabled(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void aDeviceHasBeenTurnedOn(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aDeviceHasBeenTurnedOff(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void aBarcodeHasBeenScanned(IBarcodeScanner barcodeScanner, Barcode barcode) {
+		// TODO Auto-generated method stub
+		this.barcode = barcode;
 	}
 }
