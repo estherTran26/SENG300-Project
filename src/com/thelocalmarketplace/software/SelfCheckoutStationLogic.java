8d81f732dadd4a38a86777fd18dd7e01ec85741a
estherTran26 <mtuyenucalgary@gmail.com>
2023-11-01 16:03:05 -0600

    startSession

--- src/com/thelocalmarketplace/software/SelfCheckoutStationLogic.java
+++ src/com/thelocalmarketplace/software/SelfCheckoutStationLogic.java
@@ -1,10 +1,14 @@
 package com.thelocalmarketplace.software;
 
+import com.jjjwelectronics.scanner.Barcode;
+import com.thelocalmarketplace.hardware.BarcodedProduct;
 import com.thelocalmarketplace.hardware.SelfCheckoutStation;
+import com.thelocalmarketplace.hardware.external.ProductDatabases;
 
 public class SelfCheckoutStationLogic {
 	private static boolean sessionStarts = false;
 	private static SelfCheckoutStation station;
+	private static double expectedWeight = 0;
 	
 	public SelfCheckoutStationLogic(SelfCheckoutStation st) {
 		station = st;
@@ -19,7 +23,17 @@ public class SelfCheckoutStationLogic {
 		if(simulatedStartButtonEvent()) sessionStarts = true;
 	}
 	
-	public void addItemByBarcodeScan() {
+	public void addItemByBarcodeScan(Barcode barcode) throws Exception {
+		if(!sessionStarts) throw new Exception("Cannot add item before starting a session!");
+		
+		BarcodedProduct addedProd = null;
+		
+		for (Barcode b: ProductDatabases.BARCODED_PRODUCT_DATABASE.keySet()) {
+			if(b == barcode) addedProd = ProductDatabases.BARCODED_PRODUCT_DATABASE.get(b); 
+		}
+		
+		if(addedProd == null) throw new Exception("Product not found!");
+		
 		
 	}
 }
